58f901bf7cf3c5b12e5682d5b59f564e0dcc2a46
Dheeraj Chaudhary <dheerajchaudhary231@gmail.com>
2025-08-10 18:41:49 +0530

    set up basic express server

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+    res.json({ message: "Welcome to the Mathura Darshan API!" });
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
