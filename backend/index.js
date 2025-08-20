f9ac60456b6a075fc2008655a96d6e8e5d471905
Dheeraj Chaudhary <dheerajchaudhary231@gmail.com>
2025-08-21 04:52:28 +0530

    prepare backend for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,20 @@ dotenv.config();
 connectDB();
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  process.env.FRONTEND_URL 
+];
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
 app.use(express.json());
