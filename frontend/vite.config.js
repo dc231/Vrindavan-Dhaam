cacbfcc12c6c796c2a5f1121e7c82c93100c7137
Dheeraj Chaudhary <dheerajchaudhary231@gmail.com>
2025-08-22 00:30:59 +0530

    changes in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pluginRewriteAll from '@brrock/vite-plugin-rewrite-all'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    pluginRewriteAll()
+  ],
 })
